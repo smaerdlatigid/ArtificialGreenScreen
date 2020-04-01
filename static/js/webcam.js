20461c80a5fbeae291ffe9590d697123343e8638
Kyle Pearson <pearsonk@email.arizona.edu>
2020-03-31 21:58:16 -0700

    web worker test

--- static/js/webcam.js
+++ static/js/webcam.js
@@ -1,6 +1,7 @@
 if (typeof(Worker) !== "undefined") {
     if (typeof(w) == "undefined") {
       w = new Worker("static/js/webcam_worker.js");
+      console.log("started new worker");
     }
     w.onmessage = function(event) {
         console.log( event.data);
